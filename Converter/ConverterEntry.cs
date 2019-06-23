98f13554b49ecb5f8acca35e2d497e83e9a37443
LosoncziTamas <losonczi.tamas91@gmail.com>
2019-06-23 18:26:57 +0200

    tweaking the command line argument parsing

--- Converter/ConverterEntry.cs
+++ Converter/ConverterEntry.cs
@@ -11,72 +11,76 @@ namespace Converter
     {
         class ConversionType
         {
-            private readonly string _srcFormat;
-            private readonly string _dstFormat;
-            private readonly IConversionStrategy _conversionStrategy;
+            public string SrcFormat { get; }
+            public string DstFormat { get; }
+            public IConversionStrategy ConversionStrategy { get; }
 
             public ConversionType(string srcFormat, string dstFormat, IConversionStrategy conversionStrategy)
             {
-                this._srcFormat = srcFormat;
-                this._dstFormat = dstFormat;
-                _conversionStrategy = conversionStrategy;
+                SrcFormat = srcFormat;
+                DstFormat = dstFormat;
+                ConversionStrategy = conversionStrategy;
             }
 
             public bool MatchesArguments(string argSrcFormat, string argDstFormat)
             {
-                return argSrcFormat.StartsWith(_srcFormat) && argDstFormat.StartsWith(_dstFormat);
-            }
-
-            public IConversionStrategy GetConversionStrategy()
-            {
-                return _conversionStrategy;
+                return argSrcFormat.StartsWith(SrcFormat) && argDstFormat.StartsWith(DstFormat);
             }
         }
-        class Options
+
+        class CommandLineOptions
         {
             [Option('i', "input", Required = true, HelpText = "Input filename")]
             public string InputFile { get; set; }
-        
-            [Option('o', "output", Required = false, HelpText = "Name of the output file. Input filename is used if not specified.")]
+
+            [Option('o', "output", Required = false,
+                HelpText = "Name of the output file. Input filename is used if not specified.")]
             public string OutputFile { get; set; }
-        
-            [Option('s', "srcFormat", Required = false, HelpText = "Format of the source file. Set to .obj by default.")]
+
+            [Option('s', "srcFormat", Required = false,
+                HelpText = "Format of the source file. Set to .obj by default.")]
             public string SourceFormat { get; set; }
-        
+
             [Option('d', "dstFormat", Required = false, HelpText = "Destination file format. Set to .stl by default.")]
             public string DestinationFormat { get; set; }
         }
 
-        private static readonly List<ConversionType> supportedConversions = new List<ConversionType>
+        private static readonly List<ConversionType> SupportedConversions = new List<ConversionType>
         {
             new ConversionType(".obj", ".stl", new ObjToStlConversionStrategy()),
         };
 
         public static void Main(string[] args)
         {
-            Parser.Default.ParseArguments<Options>(args)
-                .WithParsed(opts => RunOptionsAndReturnExitCode(opts))
-                .WithNotParsed((errs) => HandleParseError(errs));
+            Parser.Default.ParseArguments<CommandLineOptions>(args).WithParsed(ProcessOptions);
         }
-        
-        private static void RunOptionsAndReturnExitCode(Options opts)
+
+        private static void ProcessOptions(CommandLineOptions opts)
         {
-            var conversionType = supportedConversions.FirstOrDefault(type =>
-                type.MatchesArguments(opts.SourceFormat, opts.DestinationFormat));
+            var defaultSupportedConversion = SupportedConversions.First();
+            opts.SourceFormat = string.IsNullOrEmpty(opts.SourceFormat) ? defaultSupportedConversion.SrcFormat : opts.SourceFormat;
+            opts.DestinationFormat = string.IsNullOrEmpty(opts.DestinationFormat) ? defaultSupportedConversion.DstFormat : opts.DestinationFormat;
+            
+            var conversionType = SupportedConversions.FirstOrDefault(c => c.MatchesArguments(opts.SourceFormat, opts.DestinationFormat));
             if (conversionType != null)
             {
                 var inputPath = opts.InputFile;
-                var outputPath = string.IsNullOrEmpty(opts.OutputFile) ? "argtest.stl" : opts.OutputFile;
-                
-                conversionType.GetConversionStrategy().ApplyConversion(
-                    File.Open(inputPath, FileMode.Open), 
-                    File.Open(outputPath, FileMode.Create));
+                var outputPath = string.IsNullOrEmpty(opts.OutputFile) ? Path.GetFileNameWithoutExtension(inputPath) + opts.DestinationFormat : opts.OutputFile;
+                if (!File.Exists(inputPath))
+                {
+                    Console.WriteLine("Provided input file does not exist.");
+                    return;
+                }
+                conversionType.ConversionStrategy.ApplyConversion(File.Open(inputPath, FileMode.Open),File.Open(outputPath, FileMode.Create));
             }
-        }
-        
-        private static void HandleParseError(IEnumerable<Error> errs)
-        {
-            Console.WriteLine("handle parse error" + errs);
+            else
+            {
+                Console.WriteLine("Currently, the following conversions are supported.");
+                foreach (var conversion in SupportedConversions)
+                {
+                    Console.WriteLine("{0} to {1}", conversion.SrcFormat, conversion.DstFormat);
+                }
+            }
         }
     }
 }
